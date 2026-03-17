1228f8c92531276600d8a2211199b9e6d4cd8c08
meffxz12 <meffxz12@gmail.com>
2026-03-17 16:42:50 -0300

    gente arrumei kkk

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:meu_app/telas/bemvindo.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -7,9 +6,3 @@ import 'package:firebase_core/firebase_core.dart';
 void main() {
  runApp(MaterialApp(home: Bemvindo()));
 }
-=======
-import 'package:meu_app/cadastro.dart';
-void main() => runApp(MaterialApp(
-  home: Cadastro(),
-));
->>>>>>> a5aa2f2a5fc19b3caf905932bc132537adbd6221
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,11 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-<<<<<<< HEAD
   firebase_auth
   firebase_core
-=======
->>>>>>> a5aa2f2a5fc19b3caf905932bc132537adbd6221
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
